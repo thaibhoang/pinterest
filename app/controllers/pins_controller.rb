1f1a0eec285448c923bc57c17efc87ebd2a9ee53
thaibhoang <hoangthai.ftu53@gmail.com>
2024-04-19 00:15:24 +0700

    Allow editing pin on _created site

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -50,6 +50,7 @@ class PinsController < ApplicationController
       if @pin.update(pin_params)
         format.html { redirect_to pin_url(@pin), notice: "Pin was successfully updated." }
         format.json { render :show, status: :ok, location: @pin }
+        format.turbo_stream
       else
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @pin.errors, status: :unprocessable_entity }
