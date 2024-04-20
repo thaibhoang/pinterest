bafc0cccbf85f0dfc395f34d847a0a3fdc7dc303
thaibhoang <hoangthai.ftu53@gmail.com>
2024-04-20 11:30:38 +0700

    Add highlight for created and saved button

--- app/javascript/controllers/navigate_controller.js
+++ app/javascript/controllers/navigate_controller.js
@@ -7,8 +7,16 @@ export default class extends Controller {
   connect() {
     let path = this.getPath()
     let button_id = this.pathToTarget(path)
-    if (button_id != null) {
+    if (button_id >= 0 && button_id < 3 ) {
       this.highlight(this.navButtonTargets[button_id])
+    }
+    else if (button_id === 4 || button_id === 3 ){
+      this.highlight(this.navButtonTargets[3]);
+      this.highlightUnder(this.navButtonTargets[4]);
+    }
+    else if (button_id === 5){
+      this.highlight(this.navButtonTargets[3]);
+      this.highlightUnder(this.navButtonTargets[5]);
     }
   }
 
@@ -18,20 +26,28 @@ export default class extends Controller {
   }
 
   pathToTarget(path) {
-    if (path == "/")
-      {return 0;}
-    else if (path == "/pins/new")
-      {return 1;}
-    else if (/^\/users\/\d+\/boards\/new$/.test(path))
-      {return 2;}
-    else if (/^\/users\/\d+$/.test(path))
-      {return 3;}
-    else
-      { console.log(path);
-        return null;}
+    if (path === "/") {
+      return 0;
+    } else if (path === "/pins/new") {
+      return 1;
+    } else if (/^\/users\/\d+\/boards\/new$/.test(path)) {
+      return 2;
+    } else if (/^\/users\/\d+$/.test(path)) {
+      return 3;
+    } else if (/^\/users\/\d+\/_created$/.test(path)) {
+      return 4;
+    } else if (/^\/users\/\d+\/_saved$/.test(path)) {
+      return 5;
+    } else {
+      return null;
+    }
   }
 
   highlight(element) {
     element.classList.add("home");
+  }
+
+  highlightUnder(element) {
+    element.parentElement.classList.add("highlight-under");  
   }
 }
