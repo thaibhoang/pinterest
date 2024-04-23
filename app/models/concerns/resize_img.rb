67ef3c54fb54102cdfb9613f20009bd4740d9559
thaibhoang <hoangthai.ftu53@gmail.com>
2024-04-23 14:22:53 +0700

    Fix bug form for edit pin not sclosing after submit

--- app/models/concerns/resize_img.rb
+++ app/models/concerns/resize_img.rb
@@ -1,6 +1,6 @@
 module ResizeImg
   def resize_img (pic, size1, size2)
-    pic.variant(resize_to_limit: [size1, size2]).processed
+    pic.variant(resize_to_fit: [size1, size2]).processed
   end
 
   def resize_fill_img (pic, size1, size2)
