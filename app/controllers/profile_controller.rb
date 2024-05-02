c5fefdd2221624d7c505757dae35968f6a3ba101
thaibhoang <hoangthai.ftu53@gmail.com>
2024-05-02 08:59:42 +0700

    Clean up profile controller

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,17 +1,16 @@
+# handle profile routing
 class ProfileController < ApplicationController
   before_action :authenticate_user!, :set_user, :set_profile
   before_action :check_owner, except: %i[show]
 
-  def show     
-  end 
+  def show; end
 
-  def edit
-  end
+  def edit; end
 
   def update
     respond_to do |format|
       if @profile.update(profile_params)
-        format.html { redirect_to user_profile_url(@user, @profile)}
+        format.html { redirect_to user_profile_url(@user, @profile) }
         format.turbo_stream
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -20,15 +19,15 @@ class ProfileController < ApplicationController
   end
 
   private
-  
-  def set_user 
+
+  def set_user
     @user = User.find(params[:user_id])
   end
 
   def set_profile
     set_user
     @profile = @user.profile
-  end 
+  end
 
   def check_owner
     redicrect_to user_path(@user) if current_user != @user
