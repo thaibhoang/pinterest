5a55565359e52d0c2a679f66d6a51d42ea3da6f4
thaibhoang <hoangthai.ftu53@gmail.com>
2024-05-02 16:03:05 +0700

    Add helper method for board/_privacy_change

--- app/helpers/boards_helper.rb
+++ app/helpers/boards_helper.rb
@@ -8,6 +8,21 @@ module BoardsHelper
   end
 
   def edit_board_text(board)
-    "Edit #{board.name.truncate(20)}"
+    "Edit #{board.name.size < 20 ? board.name : 'board'}"
+  end
+
+  def render_board_image(board)
+    if board.cover_url.present?
+      link_to image_tag(board.cover_url,
+                        style: 'max-width: 300px; max-height: 600px; object-fit: contain;',
+                        class: "board-img"), board.cover_url
+    elsif board.cover.attached?
+      link_to image_tag(board.resize_img(board.cover, 300, 600), class: "board-img"), board.cover
+    end
+  end
+
+  def toggle_privacy_state(board)
+    new_privacy = !board.keep_secret
+    new_privacy ? "Public" : "Only You"
   end
 end
