bf438f5011f2a377c7ff7c011759771b09c6c0de
thaibhoang <hoangthai.ftu53@gmail.com>
2024-05-02 14:49:36 +0700

    Clean up partial board_infor

--- app/helpers/boards_helper.rb
+++ app/helpers/boards_helper.rb
@@ -1,2 +1,13 @@
 module BoardsHelper
+  def can_edit_board?(board)
+    user_signed_in? && board.user == current_user
+  end
+
+  def render_edit_board_link(board)
+    link_to edit_board_text(board), edit_user_board_path(current_user, board), class: "bold"
+  end
+
+  def edit_board_text(board)
+    "Edit #{board.name.truncate(20)}"
+  end
 end
