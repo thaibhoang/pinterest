d2e1314ff21f19ea64ed410c9037a4819a7ce7c7
thaibhoang <hoangthai.ftu53@gmail.com>
2024-05-02 12:55:20 +0700

    Clean up comment.rb

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,5 +1,5 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
   include ResizeImg
-  
+
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,12 +1,12 @@
 class Comment < ApplicationRecord
   belongs_to :pin
   belongs_to :user
-  belongs_to :parent, class_name: "Comment", optional: true
-  has_many :replies, class_name: "Comment", foreign_key: :parent_id, dependent: :destroy
+  belongs_to :parent, class_name: 'Comment', optional: true
+  has_many :replies, class_name: 'Comment', foreign_key: :parent_id, dependent: :destroy
   has_many :likes, as: :likeable
 
-  scope :is_root, -> {where(parent_id: nil) }
-  scope :newest_reply, -> {order(created_at: :desc).first}
+  scope :is_root, -> { where(parent_id: nil) }
+  scope :newest_reply, -> { order(created_at: :desc).first }
 
   validates :body, presence: true
 end
