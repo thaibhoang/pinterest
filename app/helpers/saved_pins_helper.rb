8378e75b224cfa8268e086983a408fc026ad5266
thaibhoang <hoangthai.ftu53@gmail.com>
2024-05-02 11:51:35 +0700

    Clean and move check_saved_pin helper to saved_pin helper

--- app/helpers/saved_pins_helper.rb
+++ app/helpers/saved_pins_helper.rb
@@ -1,2 +1,13 @@
+# helper method for saved_pin
 module SavedPinsHelper
+  # check if the pin has been saved to any boards, return the id of the nearest board that this pin has been saved to
+  def saved_to_board(pin, user)
+    saved_boards = user.saved_pins.where(pin: pin).order(updated_at: :desc)
+    saved_boards.present? ? [true, saved_boards.first.board_id] : [false, nil]
+  end
+
+  # return saved_pin if pin has been saved to profile else return nil
+  def check_saved_to_profile(pin, user)
+    user.saved_pins.find_by(pin: pin, board_id: nil)
+  end
 end
