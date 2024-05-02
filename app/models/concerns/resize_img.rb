dc6b58d292cea2d3a0f47e80740bc66313896611
thaibhoang <hoangthai.ftu53@gmail.com>
2024-05-02 12:47:41 +0700

    Add comment for module

--- app/models/concerns/resize_img.rb
+++ app/models/concerns/resize_img.rb
@@ -1,9 +1,10 @@
+# This module provide method for resizing image to fit or to fill a container with size1 and size2
 module ResizeImg
-  def resize_img (pic, size1, size2)
+  def resize_img(pic, size1, size2)
     pic.variant(resize_to_fit: [size1, size2]).processed
   end
 
-  def resize_fill_img (pic, size1, size2)
+  def resize_fill_img(pic, size1, size2)
     pic.variant(resize_to_fill: [size1, size2]).processed
   end
 end
