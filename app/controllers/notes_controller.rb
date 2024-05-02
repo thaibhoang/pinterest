92f91a0e2dbac111a88b9f31507af7a12adbd732
thaibhoang <hoangthai.ftu53@gmail.com>
2024-05-02 08:09:24 +0700

    Clean up notes controller

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,19 +1,18 @@
+# handling notes
 class NotesController < ApplicationController
-  before_action :authenticate_user! 
-  before_action :set_pin, only: %i[ new create ]
-  before_action :set_note, only: %i[ edit update destroy ]
-  
-  
+  before_action :authenticate_user!
+  before_action :set_pin, only: %i[new create]
+  before_action :set_note, only: %i[edit update destroy]
 
   # GET /notes/new
   def new
-    @note = @pin.notes.build   
-    @cancel = params[:cancel] ? true : false
+    @note = @pin.notes.build
+    @cancel = params[:cancel]
   end
 
   # GET /notes/1/edit
   def edit
-    @cancel = params[:cancel] ? true : false
+    @cancel = params[:cancel]
   end
 
   # POST /notes or /notes.json
@@ -23,11 +22,8 @@ class NotesController < ApplicationController
     respond_to do |format|
       if @note.save
         format.turbo_stream
-        format.html { redirect_to pin_path(@pin), notice: "Note was successfully created." }
-        format.json { render :show, status: :created, location: @note }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @note.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -37,11 +33,8 @@ class NotesController < ApplicationController
     respond_to do |format|
       if @note.update(note_params)
         format.turbo_stream
-        format.html { redirect_to pin_path(@pin), notice: "Note was successfully updated." }
-        format.json { render :show, status: :ok, location: @note }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @note.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -59,19 +52,19 @@ class NotesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_pin
-      @pin = Pin.find(params[:pin_id])
-    end
-    
-    def set_note
-      set_pin
-      @note = @pin.notes.find_by(user_id: current_user.id)
-    end
 
-    # Only allow a list of trusted parameters through.
-    def note_params
-      params.require(:note).permit(:content)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_pin
+    @pin = Pin.find(params[:pin_id])
+  end
 
+  def set_note
+    set_pin
+    @note = @pin.notes.find_by(user_id: current_user.id)
+  end
+
+  # Only allow a list of trusted parameters through.
+  def note_params
+    params.require(:note).permit(:content)
+  end
 end
