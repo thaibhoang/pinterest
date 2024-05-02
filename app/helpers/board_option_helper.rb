bbfd652f45bd0fe4baaf2f5f542f9cb4e71d9813
thaibhoang <hoangthai.ftu53@gmail.com>
2024-05-02 12:42:12 +0700

    Clean board_option_helper file

--- app/helpers/board_option_helper.rb
+++ app/helpers/board_option_helper.rb
@@ -1,16 +1,17 @@
+# helper methods for finding which boards the pin has been saved to and which it has not
 module BoardOptionHelper
-
-  def closed_boards(pin, user, boards)
-    user.saved_pins.where(pin: pin).group(:board_id).pluck(:board_id).compact.map do |board_id|
-      [Board.find(board_id).name, board_id]
-    end
+  # This return name and id of boards that pin has been saved to
+  def closed_boards(pin, user)
+    closed_board_ids = user.saved_pins.where(pin: pin).pluck(:board_id).uniq.compact
+    user.boards.where(id: closed_board_ids).pluck(:name, :id)
   end
 
-  def filter_options(pin, user, boards)   # this return two arrays, open boards raay + close boards array
+  # this return two arrays, open boards array + close boards array
+  def filter_options(pin, user, boards)
     all_boards = boards.pluck(:name, :id)
-    closed_boards = closed_boards(pin, user, boards)
+    closed_boards = closed_boards(pin, user)
     closed_board_ids = closed_boards.map(&:last)
-    open_boards = all_boards.reject { |_board_name, board_id | closed_board_ids.include?(board_id) }
-    return [open_boards, closed_boards]
+    open_boards = all_boards.reject { |_board_name, board_id| closed_board_ids.include?(board_id) }
+    [open_boards, closed_boards]
   end
 end
