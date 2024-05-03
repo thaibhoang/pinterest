c2621f52e5a656ab21b80463132710d159a9c75e
thaibhoang <hoangthai.ftu53@gmail.com>
2024-05-03 15:05:02 +0700

    Clean up comment views

--- app/helpers/likes_helper.rb
+++ app/helpers/likes_helper.rb
@@ -1,2 +1,9 @@
 module LikesHelper
+  def likes_count(likeable)
+    likeable.likes.count
+  end
+
+  def liked_by_current_user(likeable)
+    current_user.likes.find_by(likeable: likeable)
+  end
 end
