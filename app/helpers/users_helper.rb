94cb4ea5855f24c47d61cf478322977932bf54b7
thaibhoang <hoangthai.ftu53@gmail.com>
2024-05-04 11:57:17 +0700

    Clean up the users views file

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+  def show_follow_count(user)
+    count = user.followers.count
+    if count < 1000
+      pluralize(count, "follower")
+    else
+      "#{(count / 1000.0).round(1)}K followers"
+    end
+  end
 end
