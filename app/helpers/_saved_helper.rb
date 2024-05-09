77d47ff084edc3b012de19952c946cf86cdb5140
thaibhoang <hoangthai.ftu53@gmail.com>
2024-05-09 12:13:48 +0700

    Alter the look of creator_info

--- app/helpers/_saved_helper.rb
+++ app/helpers/_saved_helper.rb
@@ -2,7 +2,6 @@ module SavedHelper
   def display_when_user_has_no_boards(user)
     return unless user_signed_in? && user == current_user && user.boards.count.zero?
 
-    content_tag(:div, "You haven't created any board yet.")
-    link_to "Create One", new_user_board_path(user), data: { turbo_frame: "_top" }
+    link_to "You haven't created any board yet. Create One Here!", new_user_board_path(user), data: { turbo_frame: "_top" }
   end
 end
