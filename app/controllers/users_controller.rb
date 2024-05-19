a774c768e9818a2c86c00830524616411bd280b0
thaibhoang <hoangthai.ftu53@gmail.com>
2024-05-19 20:34:22 +0700

    Fix: image lazy loading not woring in user:show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,8 @@ class UsersController < ApplicationController
   before_action :authenticate_user!, :set_user
 
   def show
-    @pins = @user.pins
+    @pins = @user.pins.includes([:image_attachment, { saved_pins: :board }])
+    @saved_pins = current_user.saved_pins.includes(:pin).where(board_id: nil) if user_signed_in?
     respond_to do |format|
       format.turbo_stream
       format.html
